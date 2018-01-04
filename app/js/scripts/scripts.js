3a8d236b26f41f846b566f8b516332c052f14736
alexrewrew <alexrew2012@gmail.com>
2018-01-04 19:08:15 +0200

    Update 4.01

--- app/js/scripts/scripts.js
+++ app/js/scripts/scripts.js
@@ -1,206 +1,167 @@
-$(document).ready(function () {
+(function () {
     "use strict";
 
-    //  ----- FULLPAGE -----
-    $('#fullpage').fullpage({
-        // anchors: ['screen1', 'screen2', 'screen3', 'screen4', 'screen5', 'screen6', 'screen7', 'screen8', 'screen9'],
-        // menu: '#nav-menu',
-        scrollOverflow: true,
-        css3: true,
-        scrollingSpeed: 1000,
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: ['', 'Пам’ятки', 'Тури', 'Екскурсії', 'Квитки на заходи', 'Квитки на транспорт', 'Бронювання готелів', 'Розваги', 'Знижки в кафе / ресторанах', 'Бонуси за відвідані місця'],
-        onLeave: function (index, nextIndex) {
-            switch (index) {
-                case 1:
-                    $('.mask').addClass('visible');
-                    break;
-                case 10:
-                    $('.mask').removeClass('visible');
-                    break;
-                // case 10:
-                //     $('.mask').removeClass('visible');
-                //     break;
+    function createFullpage() {
+        $('#fullpage').fullpage({
+            scrollOverflow: true,
+            css3: true,
+            scrollingSpeed: 1000,
+            navigation: true,
+            navigationPosition: 'right',
+            navigationTooltips: ['', 'Пам’ятки', 'Тури', 'Екскурсії', 'Квитки на заходи', 'Квитки на транспорт', 'Бронювання готелів', 'Розваги', 'Знижки в кафе / ресторанах', 'Бонуси за відвідані місця'],
+            onLeave: function (index, nextIndex) {
+
+                switch (index) {
+                    case 2:
+                        $('.screenshot1-2').removeClass('visible');
+                        break;
+                    case 3:
+                        $('.screenshot1-3').removeClass('visible');
+                        break;
+                    case 4:
+                        $('.screenshot1-4').removeClass('visible');
+                        break;
+                    case 5:
+                        $('.screenshot1-5').removeClass('visible');
+                        break;
+                    case 6:
+                        $('.screenshot1-6').removeClass('visible');
+                        break;
+                    case 7:
+                        $('.screenshot1-7').removeClass('visible');
+                        break;
+                    case 8:
+                        $('.screenshot1-8').removeClass('visible');
+                        break;
+                    case 9:
+                        $('.screenshot1-9').removeClass('visible');
+                        break;
+                    case 10:
+                        $('.screenshot1-10').removeClass('visible');
+                        break;
+                }
+
+                switch (nextIndex) {
+                    case 1:
+                        $('.mask').removeClass('visible');
+                        $('#fp-nav').removeClass('visible');
+                        break;
+                    case 2:
+                        $('.mask').addClass('visible');
+                        $('#fp-nav').addClass('visible');
+                        $('.screenshot1-2').addClass('visible');
+                        break;
+                    case 3:
+                        $('.screenshot1-3').addClass('visible');
+                        break;
+                    case 4:
+                        $('.screenshot1-4').addClass('visible');
+                        break;
+                    case 5:
+                        $('.screenshot1-5').addClass('visible');
+                        break;
+                    case 6:
+                        $('.screenshot1-6').addClass('visible');
+                        break;
+                    case 7:
+                        $('.screenshot1-7').addClass('visible');
+                        break;
+                    case 8:
+                        $('.screenshot1-8').addClass('visible');
+                        break;
+                    case 9:
+                        $('.screenshot1-9').addClass('visible');
+                        break;
+                    case 10:
+                        $('.mask').addClass('visible');
+                        $('#fp-nav').addClass('visible');
+                        $('.screenshot1-10').addClass('visible');
+                        break;
+                    case 11:
+                        $('.mask').removeClass('visible');
+                        $('#fp-nav').removeClass('visible');
+                        break;
+                }
+            },
+            afterResize: function () {
+                $.fn.fullpage.destroy('all');
+                createFullpage();
+            }
+        });
+    }
+
+
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        createFullpage();
+    }
+
+    var destroy = false;
+    $(window).on('load resize', function () {
+
+        if (window.matchMedia("(max-width: 767px)").matches) {
+            $.fn.fullpage.destroy('all');
+            destroy = true;
+        } else if (window.matchMedia("(min-width: 768px)").matches) {
+
+            $('.mask, #fp-nav').removeClass('visible');
+
+            if (destroy) {
+                createFullpage();
+                destroy = false;
             }
 
-            // switch (nextIndex) {
-            //     case 1:
-            //         $('.mask').removeClass('visible');
-            //         break;
-            //     case 9:
-            //         $('.mask').removeClass('visible');
-            //         break;
-            //     case 10:
-            //         $('.mask').addClass('visible');
-            //         break;
-            // }
+            $(function () {
+                $('.overflow-first').slimScroll({
+                    height: 'auto'
+                });
+            });
+            $(function () {
+                $('.overflow-second').slimScroll({
+                    height: 'auto'
+                });
+            });
+
         }
-        // afterResize: function () {
-        //     $.fn.fullpage.destroy('all');
-        //     createFullpage();
-        // },
     });
 
-    // $(function () {
-    //     $('.overflow-first').slimScroll({
-    //         height: 'auto'
-    //     });
-    // });
 
-    // $(function () {
-    //     $('.overflow-second').slimScroll({
-    //         height: 'auto'
-    //     });
-    // });
+    $(document).ready(function () {
+
+        //  ----- FULLPAGE -----
 
 
-    $('.button-choice1').hover(function () {
-        $('.choice-description1').toggleClass('active')
-        $('.choice2').toggleClass('transform')
-        $('.button-choice2').toggleClass('opacity')
-    })
-    $('.button-choice2').hover(function () {
-        $('.choice-description2').toggleClass('active')
-        $('.choice1').toggleClass('transform')
-        $('.button-choice1').toggleClass('opacity')
-    })
+        // $(function () {
+        //     $('.overflow-first').slimScroll({
+        //         height: 'auto'
+        //     });
+        // });
+        //
+        // $(function () {
+        //     $('.overflow-second').slimScroll({
+        //         height: 'auto'
+        //     });
+        // });
+
+        $('#menu-trigger').click(function () {
+           $('body').toggleClass('open');
+           $('.nav-panel').slideToggle();
+
+        });
 
 
-    //  ----- SLIDER -----
-
-    // SLICK SLIDER COUNTER
-    // $('#link3 .slider').on('init reInit afterChange', function (event, slick, currentSlide) {
-    //     var i = (currentSlide ? currentSlide : 0) + 1;
-    //     $('.slider-counter').text(i + '/' + slick.slideCount);
-    // });
-    //
-    // // SLICK SLIDER
-    // $(".slider-full").slick({
-    //     // slide: ".slide",
-    //     prevArrow: "<div class='arrow left'><i class='fa fa-angle-left'></i></div>",
-    //     nextArrow: "<div class='arrow right'><i class='fa fa-angle-right'></i></div>",
-    //     dots: true
-    //
-    // });
-    //
-    // $(".slider-two").slick({
-    //     // slide: ".slide",
-    //     dots: true,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 2,
-    //     prevArrow: "<div class='arrow left'><i class='fa fa-angle-left'></i></div>",
-    //     nextArrow: "<div class='arrow right'><i class='fa fa-angle-right'></i></div>",
-    //     responsive: [
-    //         {
-    //             breakpoint: 991,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ]
-    // });
-
-
-    // $(".slider-three").slick({
-    //     // slide: ".slide",
-    //     dots: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3,
-    //     prevArrow: "<div class='arrow left'><i class='fa fa-angle-left'></i></div>",
-    //     nextArrow: "<div class='arrow right'><i class='fa fa-angle-right'></i></div>",
-    //     responsive: [
-    //         {
-    //             breakpoint: 991,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 767,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
-    //     ],
-    //
-    //
-    // });
-    //
-    // // ----- SELECT -----
-    //
-    // //CHOSEN
-    // $(".chosen-select").chosen({
-    //     disable_search_threshold: 10,
-    //     no_results_text: "Нічого не знайдено"
-    // });
-    // $(".chosen-image").chosenImage({disable_search_threshold: 10});
-
-    // ----- MODAL -----
-
-    // MODAAL PLUGIN
-    // $(".inline").modaal();
-
-    // ----- ANIMATION -----
-
-    // WOW JS
-    // new WOW().init();
-
-
-    // ----- ACCORDION -----
-    // $(function () {
-    //     $("#accordion").accordion({
-    //         collapsible: true,
-    //         heightStyle: "content"
-    //     });
-    // });
-    //
-    // // ----- TABS -----
-    // $(function () {
-    //     $("#tabs").tabs();
-    // });
-
-    // ----- SCROLLSPY -----
-
-    // $(function(){ // on document load
-    //     $('.menu').ddscrollSpy({ // initialize first demo
-    //         scrolltopoffset: -50
-    //     });
-    // });
-
-    // SCROLLING CLASS CHANGE
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 200) {
-    //         $(".link-up").addClass("visible");
-    //     }
-    //     else {
-    //         $(".link-up").removeClass("visible");
-    //     }
-    // });
-
-    // ANCHOR LINKS SCROLLING
-    $(".smooth").click(function (event) {
-        event.preventDefault();
-        var id = $(this).attr("href"),
-            top = $(id).offset().top - 70;
-        $("body,html").animate({
-            scrollTop: top
-        }, 1500);
+        $('.button-choice1').hover(function () {
+            $('.choice-description1').toggleClass('active');
+            $('.choice2').toggleClass('transform');
+            $('.button-choice2').toggleClass('opacity');
+        });
+        $('.button-choice2').hover(function () {
+            $('.choice-description2').toggleClass('active');
+            $('.choice1').toggleClass('transform');
+            $('.button-choice1').toggleClass('opacity');
+        });
     });
-});
+})();
 
-// !!! RESPONSIVE SCRIPTS !!!
 
-// $(window).on('load resize', function() {
-//     'use strict';
-//     if (window.matchMedia("(max-width: 767px)").matches) {
-//
-//     } else if (window.matchMedia("(min-width: 768px)").matches) {
-//
-//     }
-// });
 
 
